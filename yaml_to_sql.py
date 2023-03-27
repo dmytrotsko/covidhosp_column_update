9cfffa11ee306cd1c943998dea3e0560fff9948a
Dmytro Trotsko <dmytrotsko@gmail.com>
2023-03-27 18:40:25 +0300

    Added YamlReader class to wrap all yaml-related operations. Updated schemadef. Added ddl and metadata table generation functions

--- yaml_to_sql.py
+++ yaml_to_sql.py
@@ -1,72 +1,140 @@
 import yaml
 
-
 TYPES_MAPPING = {
     "int": "INT(11)",
     "float": "DOUBLE",
     "str": "VARCHAR(255)",
     "intdate": "INT(11)",
     "geocode": "VARCHAR(32)",
-    "bool": "TINYINT(1)"
+    "bool": "TINYINT(1)",
 }
 
 NEWLINE = "\n\t"
 
 
-def yaml_to_json(filename, tablename):
-    with open(f"{filename}.yaml", "r") as stream:
-        try:
-            return yaml.safe_load(stream)[tablename]
-        except yaml.YAMLError as exc:
-            print(exc)
+class YamlReader:
+    def __init__(self, schemadef_yaml_path):
+        self.schemadef_yaml_path = schemadef_yaml_path
+        self.tables_info = self.yaml_to_json()
 
+    def yaml_to_json(self):
+        with open(self.schemadef_yaml_path, "r") as f:
+            try:
+                return yaml.safe_load(f)
+            except yaml.YAMLError as exc:
+                print(exc)
 
-def create_table_statement(yaml_representation):
-    columns = []
-    for column in yaml_representation["ORDERED_CSV_COLUMNS"]:
-        if column[1] in yaml_representation["KEY_COLS"]:
-            columns.append(f"`{column[1]}` {TYPES_MAPPING[column[2]]} NOT NULL")
-        else:
-            columns.append(f"`{column[1]}` {TYPES_MAPPING[column[2]]}")
+    def get_table_info(self, entry_name):
+        table_info = self.tables_info.get(entry_name)
+        return table_info
 
-    sql_statement = f"""
-    CREATE TABLE `{yaml_representation["TABLE_NAME"]}` (
+    def get_table_key_columns(self, table_info):
+        key_columns = table_info["KEY_COLS"]
+        return key_columns
+
+    def get_table_ordered_csv_columns(self, table_info):
+        ordered_csv_columns = table_info["ORDERED_CSV_COLUMNS"]
+        return ordered_csv_columns
+
+    def get_table_unique_indexes(self, table_info):
+        unique_indexes = table_info["UNIQUE_INDEXES"]
+        return unique_indexes
+
+    def get_table_indexes(self, table_info):
+        indexes = table_info["INDEXES"]
+        return indexes
+
+    def get_table_name(self, table_info):
+        table_name = table_info["TABLE_NAME"]
+        return table_name
+
+    def generate_create_table_statement(self, entry_name):
+        table_info = self.get_table_info(entry_name)
+        columns = []
+        for column in self.get_table_ordered_csv_columns(table_info):
+            column_name = column[1] if column[2] is None else column[2]
+            column_type = TYPES_MAPPING.get(column[0])
+            not_null = "NOT NULL" if column_name in self.get_table_key_columns(table_info) else ""
+            columns.append(f"`{column_name}` {column_type} {not_null}".strip())
+
+        unique_keys = []
+        for k, v in table_info["UNIQUE_INDEXES"].items():
+            cols = ", ".join([f"`{col}`" for col in v])
+            unique_keys.append(f"UNIQUE KEY `{k}` ({cols})")
+
+        keys = []
+        for k, v in table_info["INDEXES"].items():
+            cols = ", ".join([f"`{col}`" for col in v])
+            keys.append(f"KEY `{k}` ({cols})")
+
+        sql_statement = f"""
+        CREATE TABLE `{self.get_table_name(table_info)}` (
         `id` INT NOT NULL AUTO_INCREMENT,
         `issue` INT NOT NULL,
         {f",{NEWLINE}".join([column for column in columns])},
         PRIMARY KEY (`id`),
-        UNIQUE KEY ({", ".join([f"`{column}`" for column in yaml_representation['KEY_COLS']])}),
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-    """
-    return sql_statement
+        {f",{NEWLINE}".join([u for u in unique_keys])},
+        {f",{NEWLINE}".join([k for k in keys])}
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        """
+        return sql_statement
 
-#####################################
-def create_metadata_statement(yaml_representation):
-    aggregated_key_cols = [column for column in yaml_representation["ORDERED_CSV_COLUMNS"] if column[1] in yaml_representation["AGGREGATE_KEY_COLS"]]
-    columns = []
-    for column in aggregated_key_cols:
-        if column[1] in yaml_representation["KEY_COLS"]:
-            columns.append(f"`{column[1]}` {TYPES_MAPPING[column[2]]} NOT NULL")
-        else:
-            columns.append(f"`{column[1]}` {TYPES_MAPPING[column[2]]}")
-    sql_statement = f"""
-    CREATE TABLE `{yaml_representation["TABLE_NAME"]}` (
+    def generate_create_metadata_table_statement(self, entry_name):
+        table_info = self.get_table_info(entry_name)
+        aggregated_key_cols = [
+            column
+            for column in table_info["ORDERED_CSV_COLUMNS"]
+            if column[1] in table_info["AGGREGATE_KEY_COLS"]
+        ]
+        columns = []
+        for column in aggregated_key_cols:
+            column_name = column[1] if column[2] is None else column[2]
+            column_type = TYPES_MAPPING.get(column[0])
+            not_null = "NOT NULL" if column_name in self.get_table_key_columns(table_info) else ""
+            columns.append(f"`{column_name}` {column_type} {not_null}".strip())
+
+        unique_keys = []
+        for k, v in table_info["UNIQUE_INDEXES"].items():
+            if k not in table_info["AGGREGATE_KEY_COLS"]:
+                continue
+            cols = []
+            for col in v:
+                if col in table_info["AGGREGATE_KEY_COLS"]:
+                    cols.append(col)
+            cols = ", ".join([f"`{col}`" for col in cols])
+            unique_keys.append(f"UNIQUE KEY `{k}` ({cols})")
+
+        keys = []
+        for k, v in table_info["INDEXES"].items():
+            if k not in table_info["AGGREGATE_KEY_COLS"]:
+                continue
+            cols = []
+            for col in v:
+                if col in table_info["AGGREGATE_KEY_COLS"]:
+                    cols.append(col)
+            cols = ", ".join([f"`{col}`" for col in cols])
+            keys.append(f"KEY `{k}` ({cols})")
+
+        sql_statement = f"""
+        CREATE TABLE `{self.get_table_name(table_info)}` (
         `id` INT NOT NULL AUTO_INCREMENT,
         {f",{NEWLINE}".join([column for column in columns])},
         PRIMARY KEY (`id`),
-        UNIQUE KEY ({", ".join([f"`{column}`" for column in yaml_representation['KEY_COLS']])}),
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-    """
-    return sql_statement
+        {f",{NEWLINE}".join([u for u in unique_keys])},
+        {f",{NEWLINE}".join([k for k in keys])}
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        """
+
+        return sql_statement
 
 
 def main():
-    table_name = "covid_hosp_facility"
-    table_info = yaml_to_json("covid_hosp_schemadefs", table_name)
-    create_table_ddl = create_table_statement(table_info)
-    create_medatata_ddl = create_metadata_statement(table_info)
-    print(create_table_ddl)
-    print(create_medatata_ddl)
+    yaml_reader = YamlReader("covid_hosp_schemadefs.yaml")
+    ddl = yaml_reader.generate_create_table_statement("covid_hosp_facility")
+    print(ddl)
+    metadata = yaml_reader.generate_create_metadata_table_statement("covid_hosp_facility")
+    print("*" * 20)
+    print(metadata)
 
 
 # TODO: generate covid_hosp_facility_key
